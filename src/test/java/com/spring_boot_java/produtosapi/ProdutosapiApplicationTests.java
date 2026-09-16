20709812312342cad266e7960bbd78d598bd54ff
Ramon Dantas Policarpo <ramonpolicarpo36@dinfor.uniceub>
2026-09-16 20:01:36 -0300

    Initial commit

---
+++ src/test/java/com/spring_boot_java/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_java.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
